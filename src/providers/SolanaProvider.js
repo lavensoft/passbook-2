e831da3724d031fec289c3442d63cd0ae98baa5e
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2023-08-10 15:59:48 +0700

    U Connnect

--- src/providers/SolanaProvider.js
+++ src/providers/SolanaProvider.js
@@ -1,6 +1,6 @@
-import React, { FC, useEffect, useMemo } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import { ConnectionProvider, WalletContext, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletAdapterNetwork, WalletConfigError } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
    WalletModalProvider,
@@ -9,28 +9,40 @@ import {
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { Config } from "../config";
+import SolflareClass from "@solflare-wallet/sdk";
+import { SolanaContext } from '../context';
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 export const SolanaProvider = ({ children }) => {
    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+   const [authed, setAuthed] = useState(false);
    const network = Config.NETWORK;
+   const [wallet, setWallet] = useState({});
 
    useEffect(() => {
       connect();
    }, []);
 
    const connect = async () => {
-      let wallet;
+      let solflare;
 
       try {
-         wallet = new SolflareClass({ network });
+         solflare = new SolflareClass({ network });
 
-         if (!wallet.connected) {
-            await wallet.connect();
+         if (!solflare.connected) {
+            await solflare.connect();
 
             localStorage.setItem("@user", '{"iss":"https://accounts.google.com","azp":"830626874390-hb47nkacuk4cn0c6pr788lri14s28jif.apps.googleusercontent.com","aud":"830626874390-hb47nkacuk4cn0c6pr788lri14s28jif.apps.googleusercontent.com","sub":"110196887894405140549","email":"nhatsdev@gmail.com","email_verified":"true","nbf":"1691402076","name":"Quang Nhật","picture":"https://lh3.googleusercontent.com/a/AAcHTtdo9WT6Ys6iwTVuZ2R_yDvrWNHFOt7-4MnHxNNABEHADQ=s96-c","given_name":"Quang","family_name":"Nhật","locale":"vi","iat":"1691402376","exp":"1691405976","jti":"c95b6d7264d7088b8c4f5108937ea6834e3b39f4","alg":"RS256","kid":"911e39e27928ae9f1e9d1e21646de92d19351b44","typ":"JWT"}');
+            setAuthed(true);
+
+            setWallet(wallet => (
+               {
+                  ...wallet,
+                  publicKey: solflare.publicKey
+               }
+            ))
             // window.location.href = "/#/home";
          }
       } catch (error) {
@@ -54,9 +66,12 @@ export const SolanaProvider = ({ children }) => {
             <WalletModalProvider>
                {/* <WalletMultiButton />
                <WalletDisconnectButton /> */}
-               <WalletContext.Provider>
-                  {children}
-               </WalletContext.Provider>
+               {
+                  authed  &&
+                  <SolanaContext.Provider value={wallet}>
+                     {children}
+                  </SolanaContext.Provider>
+               }
             </WalletModalProvider>
          </WalletProvider>
       </ConnectionProvider>
